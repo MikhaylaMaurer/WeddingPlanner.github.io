1fa1116c62b62242d8fd55419cd2ab94ce3285f3
Ahana <owner@owners-MacBook-Pro.local>
2024-03-19 10:38:21 -0700

    Backend update

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,8 @@ mongoose.connect(config.databaseURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
 
 var Budget = require('./WeddingPlanner');
 
